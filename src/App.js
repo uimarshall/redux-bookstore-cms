51d315fe6c1041248f075f7b16dc89379460e278
uimarshall <uimarshall@gmail.com>
2021-02-22 21:50:24 +0100

    Add linters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Welcome to bookstore</h1>
+        <h1>Welcome to bookstore</h1>
 
       </header>
     </div>
