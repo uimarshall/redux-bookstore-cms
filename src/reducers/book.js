7eb31f91d74e479bca43a67923eb1d47102487e3
uimarshall <uimarshall@gmail.com>
2021-02-23 11:20:44 +0100

    implement combineReducers

---
+++ src/reducers/book.js
@@ -0,0 +1,10 @@
+const initialState = {}
+
+const bookReducer = (state = initialState, action) => {
+  switch(action.type) {
+     default:
+       return state;
+  }
+};
+
+export default bookReducer;
