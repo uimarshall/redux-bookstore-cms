78a166f63cb5fa5c0343c74332351aa083e8433d
uimarshall <uimarshall@gmail.com>
2021-02-23 19:51:45 +0100

    Refactor BooksForm component to stateful component

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const BooksForm = () => (
-  <>
-    <form>
-      <input name="title" type="text" />
-      <select name="category">
-        {categories && categories.map(category => (
-          <option key={category}>{category}</option>
-        ))}
-      </select>
-      <button type="submit">Submit</button>
-    </form>
+class BooksForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: categories[0],
+    };
+  }
 
-  </>
-);
+  render() {
+    return (
+      <div>
+
+        <form>
+          <input name="title" type="text" />
+          <select name="category">
+            {categories && categories.map(category => (
+              <option key={category}>{category}</option>
+            ))}
+          </select>
+          <button type="submit">Submit</button>
+        </form>
+
+      </div>
+    );
+  }
+}
 
 export default BooksForm;
