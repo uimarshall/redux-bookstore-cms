43bc0d7d42feb00e6488af8f97cf2288278713c0
uimarshall <uimarshall@gmail.com>
2021-02-23 22:11:06 +0100

    Add remove book button to Book component.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book }) => (
+const Book = ({ book, removeBook }) => (
   <>
     <tr key={book.id}>
       <td>{book.id}</td>
       <td>{book.title}</td>
       <td>{book.category}</td>
+      <td>
+        <button type="submit" onClick={removeBook}>
+          Remove Book
+        </button>
+      </td>
     </tr>
 
   </>
@@ -15,6 +20,7 @@ const Book = ({ book }) => (
 Book.propTypes = {
 
   book: PropTypes.instanceOf(Object).isRequired,
+  removeBook: PropTypes.instanceOf(Function).isRequired,
 };
 
 export default Book;
