2c4ae36eb1ebd7d808ecfd49a27c79582669616d
uimarshall <uimarshall@gmail.com>
2021-02-23 13:37:24 +0100

    Adds the Redux actions and reducers

--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,7 +1,18 @@
-const initialState = {};
+import { CREATE_BOOK, REMOVE_BOOK } from '../actions/actionTypes';
+
+const initialState = {
+  book: {},
+};
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_BOOK:
+      return {
+        ...state, book: action.payload,
+
+      };
+    case REMOVE_BOOK:
+      return state.filter(book => book.id !== action.payload.id);
     default:
       return state;
   }
